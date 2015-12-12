94c12503825feb203281bcf22525b57aed7fadd5
mouylenglim <mouyleng@instedd.org>
2015-12-12 15:43:40 +0700

    add to support with sqlite plugin

--- www/js/core/db_connection.js
+++ www/js/core/db_connection.js
@@ -1,5 +1,8 @@
 function connectionDB(dbName, size) {
-  persistence.store.websql.config(
-      persistence, dbName, '0.0.1', 'Book ticket database', size, 0, 2);
-  persistence.schemaSync();
+  if (window.openDatabase || window.sqlitePlugin) {
+    persistence.store.websql.config(
+        persistence, dbName, '0.0.1', 'Book ticket database', size, 0, 2);
+    persistence.schemaSync();
+  }else
+    alert("Your device must support database connection")
 }
